193568425226d34fd0928f41fc9642692c676fec
Anderson H. G. Tabaldi <tabaldi33@gmail.com>
2021-09-03 15:16:19 -0300

    adiciona testbench do multiplexador

--- testbench.v
+++ testbench.v
@@ -1,34 +1,37 @@
-`include "registrador.v"
+`include "multiplexador.v"
 
 module testbench;
 
-    reg [15:0] entrada = 16'b0000000000000000;
-    reg clk = 0; 
-    reg load = 0;
+    reg [15:0] A = 16'b0000000000000000;
+    reg [15:0] B = 16'b0000000000000001;
+    reg [15:0] C = 16'b0000000000000010;
+    reg [15:0] D = 16'b0000000000000011;
+    reg clk = 0;
+    reg sel0 = 0; 
+    reg sel1 = 0;
     
     wire [15:0] saida;
 
-    registrador r1(entrada, clk, load, saida);
+    multiplexador m1(A, B, C, D, sel0, sel1, saida);
+
+    always #1 clk <= ~clk;
     initial begin
         $dumpvars;
-
-        entrada <= 0;
-        load <= 0;
-        #5;
-        clk <= 1;
-        #5;
-        entrada <= 1;
-        load <= 1;
-        clk <= 0;
-        #5;
-        entrada <= 1;
-        load <= 1;
-        #5;
-        clk <= 1;
-        #5;
-        entrada <= 0;
-        load <= 0;
-
+        #2;
+        sel0 <= 0;
+        sel1 <= 0;
+        #2;
+        sel0 <= 1;
+        sel1 <= 0;
+        #2;
+        sel0 <= 0;
+        sel1 <= 1;
+        #2;
+        sel0 <= 1;
+        sel1 <= 1;
+        #2;
+        sel0 <= 1;
+        sel1 <= 0;
         $finish;
     end
 endmodule

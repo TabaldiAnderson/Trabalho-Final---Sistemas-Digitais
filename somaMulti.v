0fc740ff2b2d1d3b51dc055175df64a6430ff15c
Anderson H. G. Tabaldi <tabaldi33@gmail.com>
2021-09-10 09:53:39 -0300

    adiciona ula e testbench da mesma

---
+++ somaMulti.v
@@ -0,0 +1,10 @@
+module somaMulti (
+
+    input [15:0] A, 
+    input [15:0] B, 
+    input sel,
+    output [15:0] saida
+);
+    assign saida = sel ? A * B : A + B;
+
+endmodule
--- testbench.v
+++ testbench.v
@@ -1,37 +1,29 @@
-`include "multiplexador.v"
+`include "somaMulti.v"
 
 module testbench;
 
-    reg [15:0] A = 16'b0000000000000000;
-    reg [15:0] B = 16'b0000000000000001;
-    reg [15:0] C = 16'b0000000000000010;
-    reg [15:0] D = 16'b0000000000000011;
+    reg [15:0] A = 16'b0000000000000010;
+    reg [15:0] B = 16'b0000000000000011;
     reg clk = 0;
-    reg sel0 = 0; 
-    reg sel1 = 0;
-    
+    reg sel = 0;
+
     wire [15:0] saida;
 
-    multiplexador m1(A, B, C, D, sel0, sel1, saida);
+    somaMulti m1(A, B, sel, saida);
 
     always #1 clk <= ~clk;
     initial begin
         $dumpvars;
+        #3;
+        sel <= 1;
+        #3;
+        sel <= 0;
         #2;
-        sel0 <= 0;
-        sel1 <= 0;
-        #2;
-        sel0 <= 1;
-        sel1 <= 0;
-        #2;
-        sel0 <= 0;
-        sel1 <= 1;
-        #2;
-        sel0 <= 1;
-        sel1 <= 1;
-        #2;
-        sel0 <= 1;
-        sel1 <= 0;
+        A <= 16'b0000000000000100;
+        B <= 16'b0000000000000010;
+        #3;
+        sel <= 1;
+        #3
         $finish;
     end
 endmodule

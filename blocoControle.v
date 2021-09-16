3343f4ca105b45f7df1c76a9f3bfbb2d9203a07b
Anderson H. G. Tabaldi <tabaldi33@gmail.com>
2021-09-15 21:04:45 -0300

    altera bloco de controle e adiciona testbench do mesmo

--- blocoControle.v
+++ blocoControle.v
@@ -1,10 +1,10 @@
-module BlocoControle (
+module blocoControle (
     input inicio,
     input clk,
     input rst,
     output [1:0] M0,
-    output [1:0] M0,
-    output [1:0] M0,
+    output [1:0] M1,
+    output [1:0] M2,
     output LX,
     output LH, 
     output LS,
@@ -14,11 +14,11 @@ module BlocoControle (
 
     reg [3:0] estado;
 
-    always @(posedge clk || reset) begin
+    always @(posedge clk or rst) begin
         if (rst)
             estado <= 0;
         else begin
-            if (estado == 0 && inicio == 0)
+            if (estado == 0 && ~inicio)
                 estado <= estado;
             else begin
                 if (pronto)
@@ -36,8 +36,8 @@ module BlocoControle (
     assign LX = estado == 1 ? 1 : 0;
     assign LH = estado == 3 ? 1 : estado == 5 ? 1 : 0;
     assign H = estado == 1 ? 1 : estado == 2 ? 1 : estado == 3 ? 1 : estado == 4 ? 1 : estado == 5 ? 1 : 0;
-    assign M0 = estado == 4 ? 1 : estado == 5 ? 2 : estado == 6 ? 3 : estado == 7 ? 3 : 0;
-    assign M1 = estado == 1 ? 1 : estado == 2 ? 1 : estado == 3 ? 1 : estado == 5 ? 1 : estado == 4 ? 2 : estado == 6 ? 2 : 0;  
+    assign M0 = estado == 4 ? 1 : estado == 5 ? 2 : estado == 7 ? 3 : 0;
+    assign M1 = estado == 1 ? 1 : estado == 2 ? 1 : estado == 3 ? 1 : estado == 5 ? 1 : estado == 4 ? 3 : estado == 6 ? 3 : 0;  
     assign M2 = estado == 4 ? 1 : estado == 6 ? 2 : estado == 7 ? 2 : 0;
     assign pronto = estado == 8 ? 1 : 0;
 
--- testbench.v
+++ testbench.v
@@ -1,65 +1,33 @@
-`include "blocoOperativo.v"
+`include "blocoControle.v"
 
 module testbench;
 
-    reg [15:0] A = 16'b0000000000000000;
-    reg [15:0] B = 16'b0000000000000000;
-    reg [15:0] C = 16'b0000000000000000;
-    reg [7:0] K = 8'b00000100;
-    reg LX = 0;
-    reg LH = 0;
-    reg LS = 0;
-    reg [1:0] M0 = 2'b00;
-    reg [1:0] M1 = 2'b00; 
-    reg [1:0] M2 = 2'b00;
-    reg H = 0;
+    reg inicio = 0;
     reg clk = 0;
-    wire [15:0] resultado;
+    reg rst = 0;
+    //reg [3:0] estado = 4'b0000; 
+    wire [1:0] M0;
+    wire [1:0] M1; 
+    wire [1:0] M2;
+    wire LX;
+    wire LH;
+    wire LS;
+    wire H;
+    wire pronto;
 
-    blocoOperativo bloco1(A, B, C, K, LX, LH, LS, M0, M1, M2, H, clk, resultado);           
+    blocoControle bloco1(inicio, clk, rst, M0, M1, M2, LX, LH, LS, H, pronto);
 
-    always #1 clk <= ~clk;
+    always #1 clk <= ~clk;         
+
+    //always #1 clk <= ~clk;
 
     initial begin
         $dumpvars;
-        LX = 1;
-        M1 = 01;
-        M2 = 00;
-        H = 1;
-        LS = 0;
-        LH = 0;
+        rst <= 1;
         #1;
-        LX = 0;
-        M1 = 01;
-        M2 = 00;
-        H = 1;
-        LS = 0;
-        LH = 0;
-        #1;
-        LX = 0;
-        M0 = 0;
-        M1 = 01;
-        M2 = 00;
-        H = 1;
-        LS = 0;
-        LH = 1;
-        #1;
-        LX = 0;
-        M0 = 01;
-        M1 = 11;
-        M2 = 01;
-        H = 1;
-        LS = 1;
-        LH = 0;
-        #1;
-        LX = 0;
-        M0 = 10;
-        M1 = 01;
-        M2 = 00;
-        H = 1;
-        LS = 0;
-        LH = 1;
-        #1;
+        rst <= 0;
+        inicio <= 1;
+        #15;
         $finish;
     end
 endmodule
